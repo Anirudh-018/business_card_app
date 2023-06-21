23e3195e615953ad9911e4a11d8146b158e179a0
Anirudh r k <menonani7@gmail.com>
2023-06-21 20:40:16 +0530

    business card app initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp()
   );
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            color: Colors.black45,
+            child: Text("container 1"),
+          ),
+        ),
+      ),
+    );
+  }
 }
