bc987293bdd49270f643af61092ed5f895039814
Anirudh r k <menonani7@gmail.com>
2023-06-24 12:39:50 +0530

    column layout

--- lib/main.dart
+++ lib/main.dart
@@ -10,29 +10,83 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.black87,
         body: SafeArea(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.black45,
-                child: Text("container 1"),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('assets/images/ani_photo.JPG'),
               ),
-              Container(
-                height: 100.0,
-                width: 100.0,
+              Text(
+                'ANIRUDH R K',
+                style: TextStyle(
+                  color:Colors.redAccent,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Fjalla One'),
+              ),
+              Text(
+                'FLUTTER AND BACKEND DEVELOPER',
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 10.0
+                ),
+              ),
+              Divider(
+                indent: 100.0,
+                endIndent: 100.0,
                 color: Colors.blue,
-                child: Text("container 2"),
               ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.brown,
-                child: Text("container 3"),
+              Card(
+                color: Colors.blueGrey,
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal:10.0),
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.white70,
+                      ),
+                      SizedBox(
+                        width: 15.0,
+                      ),
+                      Text(
+                        "+91 7010475021",
+                        style: TextStyle(
+                          color: Colors.redAccent
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ),
+              Card(
+                color: Colors.blueGrey,
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal:10.0),
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.white70,
+                      ),
+                      SizedBox(
+                        width: 15.0,
+                      ),
+                      Text(
+                        "menonani7@gmail.com",
+                        style: TextStyle(
+                            color: Colors.red
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         ),
